69ed922370bb85077778e085844366cf1e9b6632
technologicaljerry <cserajatverma@gmail.com>
2023-05-28 23:55:31 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        Hello, Welcpme to full stack NextJs Application.
       </h1>
     </main>
   )
